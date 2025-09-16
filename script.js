cec7681c7edc857f238cd7fa9e0620d48172608b
AdrianMaciel <a22adrmacfir@inspedralbes.cat>
2025-09-16 09:56:30 +0200

    Actualitzacio script.js

--- script.js
+++ script.js
@@ -1,19 +1,24 @@
 import data from "./data.js";
 //alert("hola");
-console.log(data);
-let contenidor = document.getElementById("questionari");
 
+let contenidor = document.getElementById("questionari");
 let htmlString="";
 
 for (let i = 0; i < data.preguntes.length; i++) {
-    htmlString += `<h3> ${data.preguntes[i].pregunta} </h3>`;
-    htmlString += `<img src="img/${data.preguntes[i].imatge}" alt="imatge pregunta ${i+1}"> <br>`;
-
-    for (let j = 0; j < data.preguntes[i].respostes.length; j++){
-        htmlString += `<button onclick="marcarResposta(${i}, ${j})">
-        ${data.preguntes[i].respostes[j].resposta}
-        </button> `;
+    const pregunta = data.preguntes[i];
+  
+    htmlString += `<h3>${pregunta.pregunta}</h3>`;
+    htmlString += `<img src="${pregunta.imatge}" alt="imatge pregunta ${i + 1}" style="max-width:200px;display:block;margin:1rem auto;">`;
+  
+    for (let j = 0; j < pregunta.respostes.length; j++) {
+      htmlString += `
+        <button id="btn-${i}-${j}" onclick="marcarResposta(${i},${j})">
+          ${pregunta.respostes[j].resposta}
+        </button>
+      `;
     }
-}
+  
+    htmlString += "<hr>";
+  }
 
 contenidor.innerHTML = htmlString;
