e164f8c49f272efcd32117b9025169cfabadac00
Adrian Maciel <a22adrmacfir@inspedralbes.cat>
2025-09-16 12:29:53 +0200

    Insercio d'imatges a les respostes en data.js

--- data.js
+++ data.js
@@ -14,190 +14,189 @@ export default {
       id: 2,
       pregunta: "Quin és el logotip d’Apple?",
       respostes: [
-        { id: 1, text: "Apple", correcta: true, imatge: "https://simpleicons.org/icons/apple.svg" },
-        { id: 2, text: "Samsung", correcta: false, imatge: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTh9axExasKJoaXSnBnr8vc25f-zDUuHSvh4A&s" },
-        { id: 3, text: "Sony", correcta: false, imatge: "https://pluspng.com/logo-img/so142sond681-sony-logo-sony-group-portal-home.png" },
-        { id: 4, text: "Huawei", correcta: false, imatge: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSH9wxO5Dk0EyiRvjbFNx5yUo-DqP7kQ9zzqQ&s" }
+        { id: 1, text: "", correcta: true, imatge: "https://api.triviacreator.com/v1/imgs/quiz/8a7240d179-450-ceca425e-64e9-4495-94ac-53985e96b9d3.png" },
+        { id: 2, text: "", correcta: false, imatge: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTh9axExasKJoaXSnBnr8vc25f-zDUuHSvh4A&s" },
+        { id: 3, text: "", correcta: false, imatge: "https://pluspng.com/logo-img/so142sond681-sony-logo-sony-group-portal-home.png" },
+        { id: 4, text: "", correcta: false, imatge: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSH9wxO5Dk0EyiRvjbFNx5yUo-DqP7kQ9zzqQ&s" }
       ]
     },
     {
       id: 3,
       pregunta: "Quin logotip pertany a Coca-Cola?",
       respostes: [
-        { id: 1, text: "Coca-Cola", correcta: true, imatge: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQifVv8M286hSVl88wSgnt_audHp2tJ0o97Qg&s" },
-        { id: 2, text: "Pepsi", correcta: false, imatge: "" },
-        { id: 3, text: "Dr Pepper", correcta: false, imatge: "" },
-        { id: 4, text: "7UP", correcta: false, imatge: ""}
+        { id: 1, text: "", correcta: true, imatge: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSdHM3pgI5wMfDnAqgDX6ld2ALPRRijJ00s4w&s" },
+        { id: 2, text: "", correcta: false, imatge: "https://i.pinimg.com/474x/d1/23/a6/d123a699d889bf2ea3543a56f2ce18cf.jpg" },
+        { id: 3, text: "", correcta: false, imatge: "https://pbs.twimg.com/media/E7efFYfXMAY66w9.png" },
+        { id: 4, text: "", correcta: false, imatge: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRObb3MpBSl4erlkFnBwTz_h6irckSpBSSQnQ&s"}
       ]
     },
     {
       id: 4,
       pregunta: "Quin és el logotip de Nike?",
       respostes: [
-        { id: 1, text: "Nike", correcta: true, imatge: "https://upload.wikimedia.org/wikipedia/commons/a/a6/Logo_NIKE.svg" },
-        { id: 2, text: "Adidas", correcta: false, imatge: "https://upload.wikimedia.org/wikipedia/commons/2/20/Adidas_Logo.svg" },
-        { id: 3, text: "Puma", correcta: false, imatge: "https://upload.wikimedia.org/wikipedia/commons/f/fd/Puma_AG.svg" },
-        { id: 4, text: "Reebok", correcta: false, imatge: "https://upload.wikimedia.org/wikipedia/commons/2/2a/Reebok_2019_logo.svg" }
+        { id: 1, text: "", correcta: true, imatge: "https://upload.wikimedia.org/wikipedia/commons/a/a6/Logo_NIKE.svg" },
+        { id: 2, text: "", correcta: false, imatge: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTOXBUSqR9QfqToTSuR2VKYa4UOm4ojiJdn8A&s" },
+        { id: 3, text: "", correcta: false, imatge: "https://i.pinimg.com/originals/ca/b4/1b/cab41b8b072053584102f8567f6ecce5.jpg" },
+        { id: 4, text: "", correcta: false, imatge: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSmWl9lUsbbzP5vM52R00xIgoVorht_UYOR4w&s" }
       ]
     },
     {
       id: 5,
       pregunta: "Quin logotip representa Starbucks?",
       respostes: [
-        { id: 1, text: "Starbucks", correcta: true, imatge: "https://upload.wikimedia.org/wikipedia/en/d/d3/Starbucks_Corporation_Logo_2011.svg" },
-        { id: 2, text: "Costa Coffee", correcta: false, imatge: "https://upload.wikimedia.org/wikipedia/commons/5/57/Costa_Coffee_logo.svg" },
-        { id: 3, text: "Nespresso", correcta: false, imatge: "https://upload.wikimedia.org/wikipedia/commons/1/1a/Nespresso_logo.svg" },
-        { id: 4, text: "Dunkin'", correcta: false, imatge: "https://upload.wikimedia.org/wikipedia/commons/7/7e/Dunkin%27_Logo.svg" }
-      ]
+        { id: 1, text: "", correcta: true, imatge: "https://upload.wikimedia.org/wikipedia/en/d/d3/Starbucks_Corporation_Logo_2011.svg" },
+        { id: 2, text: "", correcta: false, imatge: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRsaVtFtTGmzHiAGBh4S2jt4kjzgweab_ytsA&s" },
+        { id: 3, text: "", correcta: false, imatge: "https://i.pinimg.com/736x/a4/28/5f/a4285fd266a5e705678fcda933519df7.jpg" },
+        { id: 4, text: "", correcta: false, imatge: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQZz5kgElf_8HVk5EJnsD7k64j01_RMZzQofQ&s" }      ]
     },
     {
       id: 6,
-      pregunta: "Quin logotip és de Sony (PlayStation)?",
+      pregunta: "Quin logotip és de PlayStation?",
       respostes: [
-        { id: 1, text: "Sony", correcta: true, imatge: "https://upload.wikimedia.org/wikipedia/commons/2/25/Sony_logo.svg" },
-        { id: 2, text: "Microsoft", correcta: false, imatge: "https://upload.wikimedia.org/wikipedia/commons/4/44/Microsoft_logo.svg" },
-        { id: 3, text: "Nintendo", correcta: false, imatge: "https://upload.wikimedia.org/wikipedia/commons/0/0d/Nintendo.svg" },
-        { id: 4, text: "Sega", correcta: false, imatge: "https://upload.wikimedia.org/wikipedia/commons/6/64/SEGA_logo.svg" }
+        { id: 1, text: "", correcta: true, imatge: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSH8wt-79v8mHxlRJEoxLqTnHhMsbgJVgGCTw&s" },
+        { id: 2, text: "", correcta: false, imatge: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRiEM_izokj80kOWvN9B8eDQfGy6MLZU0icWw&s" },
+        { id: 3, text: "", correcta: false, imatge: "https://media.baamboozle.com/uploads/images/410104/1640753043_9758.jpeg" },
+        { id: 4, text: "", correcta: false, imatge: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQG1lqrBAYLmYdkGcOjuJr7Ix-BEwSsRTr9og&s" }
       ]
     },
     {
       id: 7,
       pregunta: "Quin logotip pertany a Google?",
       respostes: [
-        { id: 1, text: "Google", correcta: true, imatge: "https://upload.wikimedia.org/wikipedia/commons/2/2f/Google_2015_logo.svg" },
-        { id: 2, text: "Yahoo", correcta: false, imatge: "https://upload.wikimedia.org/wikipedia/commons/2/24/Yahoo%21_logo.svg" },
-        { id: 3, text: "Bing", correcta: false, imatge: "https://upload.wikimedia.org/wikipedia/commons/9/9c/Bing_logo_2020.svg" },
-        { id: 4, text: "DuckDuckGo", correcta: false, imatge: "https://upload.wikimedia.org/wikipedia/en/4/4a/DuckDuckGo_logo.svg" }
+        { id: 1, text: "", correcta: true, imatge: "https://quizizz.com/media/resource/gs/quizizz-media/quizzes/940f318e-d7e6-4277-ae39-0723d02373c7" },
+        { id: 2, text: "", correcta: false, imatge: "https://www.jetpunk.com/img/user-img/b2/b25be27e03-450.webp" },
+        { id: 3, text: "", correcta: false, imatge: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQSLFS48ZDFfFjDnzM4zU9BEbrU-KUvK3I0LQ&s" },
+        { id: 4, text: "", correcta: false, imatge: "https://logos-world.net/wp-content/uploads/2022/04/DuckDuckGo-Emblem.png" }
       ]
     },
     {
       id: 8,
       pregunta: "Quin és el logotip correcte d’Audi?",
       respostes: [
-        { id: 1, text: "Audi", correcta: true, imatge: "https://upload.wikimedia.org/wikipedia/commons/6/6f/Audi_logo_detail.svg" },
-        { id: 2, text: "Volkswagen", correcta: false, imatge: "https://upload.wikimedia.org/wikipedia/commons/2/24/VW_Logo.svg" },
-        { id: 3, text: "Skoda", correcta: false, imatge: "https://upload.wikimedia.org/wikipedia/commons/8/87/Skoda_Auto_logo_green.svg" },
-        { id: 4, text: "Seat", correcta: false, imatge: "https://upload.wikimedia.org/wikipedia/commons/e/ef/SEAT_Logo.svg" }
+        { id: 1, text: "Audi", correcta: true, imatge: "" },
+        { id: 2, text: "Volkswagen", correcta: false, imatge: "" },
+        { id: 3, text: "Skoda", correcta: false, imatge: "" },
+        { id: 4, text: "Seat", correcta: false, imatge: "" }
       ]
     },
     {
       id: 9,
       pregunta: "Quin d’aquests logos és Burberry?",
       respostes: [
-        { id: 1, text: "Burberry", correcta: true, imatge: "https://upload.wikimedia.org/wikipedia/commons/8/87/Burberry_logo.svg" },
-        { id: 2, text: "Gucci", correcta: false, imatge: "https://upload.wikimedia.org/wikipedia/commons/6/62/Gucci_logo.svg" },
-        { id: 3, text: "Prada", correcta: false, imatge: "https://upload.wikimedia.org/wikipedia/commons/6/6a/Prada_logo.svg" },
-        { id: 4, text: "Louis Vuitton", correcta: false, imatge: "https://upload.wikimedia.org/wikipedia/commons/f/fd/Louis_Vuitton_logo_and_wordmark.svg" }
+        { id: 1, text: "Burberry", correcta: true, imatge: "" },
+        { id: 2, text: "Gucci", correcta: false, imatge: "" },
+        { id: 3, text: "Prada", correcta: false, imatge: "" },
+        { id: 4, text: "Louis Vuitton", correcta: false, imatge: "" }
       ]
     },
     {
       id: 10,
       pregunta: "Quin logotip és de Samsung?",
       respostes: [
-        { id: 1, text: "Samsung", correcta: true, imatge: "https://upload.wikimedia.org/wikipedia/commons/2/24/Samsung_Logo.svg" },
-        { id: 2, text: "Huawei", correcta: false, imatge: "https://upload.wikimedia.org/wikipedia/commons/6/66/Huawei_Standard_logo.svg" },
-        { id: 3, text: "Apple", correcta: false, imatge: "https://upload.wikimedia.org/wikipedia/commons/f/fa/Apple_logo_black.svg" },
-        { id: 4, text: "Xiaomi", correcta: false, imatge: "https://upload.wikimedia.org/wikipedia/commons/2/29/Xiaomi_logo.svg" }
+        { id: 1, text: "Samsung", correcta: true, imatge: "" },
+        { id: 2, text: "Huawei", correcta: false, imatge: "" },
+        { id: 3, text: "Apple", correcta: false, imatge: "" },
+        { id: 4, text: "Xiaomi", correcta: false, imatge: "" }
       ]
     },
     {
       id: 11,
       pregunta: "Quin logotip representa Red Bull?",
       respostes: [
-        { id: 1, text: "Red Bull", correcta: true, imatge: "https://upload.wikimedia.org/wikipedia/en/thumb/4/4c/Red_Bull_logo.svg/512px-Red_Bull_logo.svg.png" },
-        { id: 2, text: "Monster", correcta: false, imatge: "https://upload.wikimedia.org/wikipedia/commons/e/e1/Monster_Energy_logo.svg" },
-        { id: 3, text: "Rockstar", correcta: false, imatge: "https://upload.wikimedia.org/wikipedia/en/5/5a/Rockstar_Energy_Drink_logo.svg" },
-        { id: 4, text: "Burn", correcta: false, imatge: "https://upload.wikimedia.org/wikipedia/commons/d/d7/Burn_logo.svg" }
+        { id: 1, text: "Red Bull", correcta: true, imatge: "" },
+        { id: 2, text: "Monster", correcta: false, imatge: "" },
+        { id: 3, text: "Rockstar", correcta: false, imatge: "" },
+        { id: 4, text: "Burn", correcta: false, imatge: "" }
       ]
     },
     {
       id: 12,
       pregunta: "Quin és el logotip de Rolex?",
       respostes: [
-        { id: 1, text: "Rolex", correcta: true, imatge: "https://upload.wikimedia.org/wikipedia/commons/9/95/Rolex_logo.svg" },
-        { id: 2, text: "Omega", correcta: false, imatge: "https://upload.wikimedia.org/wikipedia/commons/0/0e/Omega_SA_logo.svg" },
-        { id: 3, text: "Tag Heuer", correcta: false, imatge: "https://upload.wikimedia.org/wikipedia/commons/1/19/TAG_Heuer_logo.svg" },
-        { id: 4, text: "Patek Philippe", correcta: false, imatge: "https://upload.wikimedia.org/wikipedia/commons/7/79/Patek_Philippe_logo.svg" }
+        { id: 1, text: "Rolex", correcta: true, imatge: "" },
+        { id: 2, text: "Omega", correcta: false, imatge: "" },
+        { id: 3, text: "Tag Heuer", correcta: false, imatge: "" },
+        { id: 4, text: "Patek Philippe", correcta: false, imatge: "" }
       ]
     },
     {
       id: 13,
       pregunta: "Quin d’aquests és Calvin Klein?",
       respostes: [
-        { id: 1, text: "Calvin Klein", correcta: true, imatge: "https://upload.wikimedia.org/wikipedia/commons/8/88/Calvin_klein_logo.svg" },
-        { id: 2, text: "Tommy Hilfiger", correcta: false, imatge: "https://upload.wikimedia.org/wikipedia/commons/4/4c/Tommy_Hilfiger_logo.svg" },
-        { id: 3, text: "Hugo Boss", correcta: false, imatge: "https://upload.wikimedia.org/wikipedia/commons/4/41/Hugo-Boss-Logo.svg" },
-        { id: 4, text: "Guess", correcta: false, imatge: "https://upload.wikimedia.org/wikipedia/commons/3/35/Guess_logo.svg" }
+        { id: 1, text: "Calvin Klein", correcta: true, imatge: "" },
+        { id: 2, text: "Tommy Hilfiger", correcta: false, imatge: "" },
+        { id: 3, text: "Hugo Boss", correcta: false, imatge: "" },
+        { id: 4, text: "Guess", correcta: false, imatge: "" }
       ]
     },
     {
       id: 14,
       pregunta: "Quin logotip és d’Hermès?",
       respostes: [
-        { id: 1, text: "Hermès", correcta: true, imatge: "https://upload.wikimedia.org/wikipedia/commons/d/d9/Herm%C3%A8s_logo.svg" },
-        { id: 2, text: "Hugo Boss", correcta: false, imatge: "https://upload.wikimedia.org/wikipedia/commons/4/41/Hugo-Boss-Logo.svg" },
-        { id: 3, text: "Honda", correcta: false, imatge: "https://upload.wikimedia.org/wikipedia/commons/1/19/Honda-logo.svg" },
-        { id: 4, text: "Hyundai", correcta: false, imatge: "https://upload.wikimedia.org/wikipedia/commons/4/45/Hyundai_logo.svg" }
+        { id: 1, text: "Hermès", correcta: true, imatge: "" },
+        { id: 2, text: "Hugo Boss", correcta: false, imatge: "" },
+        { id: 3, text: "Honda", correcta: false, imatge: "" },
+        { id: 4, text: "Hyundai", correcta: false, imatge: "" }
       ]
     },
     {
       id: 15,
       pregunta: "Quin logotip és Toblerone?",
       respostes: [
-        { id: 1, text: "Toblerone", correcta: true, imatge: "https://upload.wikimedia.org/wikipedia/commons/2/23/Toblerone_logo.svg" },
-        { id: 2, text: "Lindt", correcta: false, imatge: "https://upload.wikimedia.org/wikipedia/commons/4/4a/Lindt_Logo.svg" },
-        { id: 3, text: "Milka", correcta: false, imatge: "https://upload.wikimedia.org/wikipedia/commons/4/4c/Milka_logo.svg" },
-        { id: 4, text: "Nestlé", correcta: false, imatge: "https://upload.wikimedia.org/wikipedia/commons/f/fb/Nestle_textlogo.svg" }
+        { id: 1, text: "Toblerone", correcta: true, imatge: "" },
+        { id: 2, text: "Lindt", correcta: false, imatge: "" },
+        { id: 3, text: "Milka", correcta: false, imatge: "" },
+        { id: 4, text: "Nestlé", correcta: false, imatge: "" }
       ]
     },
     {
       id: 16,
       pregunta: "Quin logotip és de L’Oréal Paris?",
       respostes: [
-        { id: 1, text: "L’Oréal Paris", correcta: true, imatge: "https://upload.wikimedia.org/wikipedia/commons/b/b3/L%27Oreal_logo.svg" },
-        { id: 2, text: "Maybelline", correcta: false, imatge: "https://upload.wikimedia.org/wikipedia/commons/5/5c/Maybelline_Logo.svg" },
-        { id: 3, text: "Revlon", correcta: false, imatge: "https://upload.wikimedia.org/wikipedia/commons/5/5f/Revlon_logo.svg" },
-        { id: 4, text: "Clinique", correcta: false, imatge: "https://upload.wikimedia.org/wikipedia/commons/d/dc/Clinique_logo.svg" }
+        { id: 1, text: "L’Oréal Paris", correcta: true, imatge: "" },
+        { id: 2, text: "Maybelline", correcta: false, imatge: "" },
+        { id: 3, text: "Revlon", correcta: false, imatge: "" },
+        { id: 4, text: "Clinique", correcta: false, imatge: "" }
       ]
     },
     {
       id: 17,
       pregunta: "Quin logotip és d’Adidas?",
       respostes: [
-        { id: 1, text: "Adidas", correcta: true, imatge: "https://upload.wikimedia.org/wikipedia/commons/2/20/Adidas_Logo.svg" },
-        { id: 2, text: "Nike", correcta: false, imatge: "https://upload.wikimedia.org/wikipedia/commons/a/a6/Logo_NIKE.svg" },
-        { id: 3, text: "Puma", correcta: false, imatge: "https://upload.wikimedia.org/wikipedia/commons/f/fd/Puma_AG.svg" },
-        { id: 4, text: "Asics", correcta: false, imatge: "https://upload.wikimedia.org/wikipedia/commons/0/03/Asics_Logo.svg" }
+        { id: 1, text: "Adidas", correcta: true, imatge: "" },
+        { id: 2, text: "Nike", correcta: false, imatge: "" },
+        { id: 3, text: "Puma", correcta: false, imatge: "" },
+        { id: 4, text: "Asics", correcta: false, imatge: "" }
       ]
     },
     {
       id: 18,
       pregunta: "Quin logotip és Louis Vuitton?",
       respostes: [
-        { id: 1, text: "Louis Vuitton", correcta: true, imatge: "https://upload.wikimedia.org/wikipedia/commons/f/fd/Louis_Vuitton_logo_and_wordmark.svg" },
-        { id: 2, text: "Gucci", correcta: false, imatge: "https://upload.wikimedia.org/wikipedia/commons/6/62/Gucci_logo.svg" },
-        { id: 3, text: "Fendi", correcta: false, imatge: "https://upload.wikimedia.org/wikipedia/commons/b/b9/Fendi_logo.svg" },
-        { id: 4, text: "Dior", correcta: false, imatge: "https://upload.wikimedia.org/wikipedia/commons/f/fc/Dior_Logo.svg" }
+        { id: 1, text: "Louis Vuitton", correcta: true, imatge: "" },
+        { id: 2, text: "Gucci", correcta: false, imatge: "" },
+        { id: 3, text: "Fendi", correcta: false, imatge: "" },
+        { id: 4, text: "Dior", correcta: false, imatge: "" }
       ]
     },
     {
       id: 19,
       pregunta: "Quin logotip és Corona (cervesa)?",
       respostes: [
-        { id: 1, text: "Corona", correcta: true, imatge: "https://upload.wikimedia.org/wikipedia/commons/7/73/Corona_Extra_logo.svg" },
-        { id: 2, text: "Heineken", correcta: false, imatge: "https://upload.wikimedia.org/wikipedia/commons/b/b5/Heineken_logo.svg" },
-        { id: 3, text: "Budweiser", correcta: false, imatge: "https://upload.wikimedia.org/wikipedia/commons/3/31/Budweiser_Logo.svg" },
-        { id: 4, text: "Stella Artois", correcta: false, imatge: "https://upload.wikimedia.org/wikipedia/commons/3/31/Stella_Artois_logo.svg" }
+        { id: 1, text: "Corona", correcta: true, imatge: "" },
+        { id: 2, text: "Heineken", correcta: false, imatge: "" },
+        { id: 3, text: "Budweiser", correcta: false, imatge: "" },
+        { id: 4, text: "Stella Artois", correcta: false, imatge: "" }
       ]
     },
     {
       id: 20,
       pregunta: "Quin logotip és Mitsubishi?",
       respostes: [
-        { id: 1, text: "Mitsubishi", correcta: true, imatge: "https://upload.wikimedia.org/wikipedia/commons/6/66/Mitsubishi_logo.svg" },
-        { id: 2, text: "Mazda", correcta: false, imatge: "https://e7.pngegg.com/pngimages/634/298/png-clipart-mazda-mx-5-car-logo-mazda-cdr-logo.png"},
-        { id: 3, resposta: "Toyota", correcta: false, imatge:"https://w1.pngwing.com/pngs/938/341/png-transparent-toyota-logo-toyota-hiace-car-toyota-corolla-toyota-prius-toyota-highlander-toyota-camry-vehicle.png"},
-        { id: 4, resposta: "Suzuki", correcta: false, imatge: "https://images.seeklogo.com/logo-png/13/2/suzuki-logo-png_seeklogo-134277.png"}
+        { id: 1, text: "Mitsubishi", correcta: true, imatge: "" },
+        { id: 2, text: "Mazda", correcta: false, imatge: ""},
+        { id: 3, resposta: "Toyota", correcta: false, imatge:""},
+        { id: 4, resposta: "Suzuki", correcta: false, imatge: ""}
       ]
     }
   ]
